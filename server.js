ffa9f066008c8ac1fd1ed253b31c420693cb63a9
Stella <stellaclough@gmail.com>
2025-02-07 09:54:19 +0100

    added picture <3

--- server.js
+++ server.js
@@ -72,3 +72,4 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
